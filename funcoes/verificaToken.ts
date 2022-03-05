209ddeeb8c67e25cd991035c79d98b46834e7381
Eduardo Manoel de Lara <52054453+Eduardo2712@users.noreply.github.com>
2022-03-05 12:57:39 -0300

    Atualização

--- funcoes/verificaToken.ts
+++ funcoes/verificaToken.ts
@@ -3,29 +3,32 @@ import { Request, Response } from "express";
 const jwt = require("jsonwebtoken");
 
 class VerificaToken {
-    static verificaToken = async (req: Request, res: Response, prox: any) => {
+    static verificaToken = async (
+        req: Request,
+        res: Response,
+        prox: () => {}
+    ) => {
         const token = req.headers["x-access-token"];
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.header(
-            "Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept"
-        );
         if (!token) {
             return res.status(401).json({
                 autenticado: false,
                 mensagem: "Token não encontrado",
             });
         }
-        jwt.verify(token, process.env.CHAVE_TOKEN, (err: any, decoder: any) => {
-            if (err) {
-                return res.status(401).json({
-                    autenticado: false,
-                    mensagem: "Usuário não autorizado",
-                });
+        jwt.verify(
+            token,
+            process.env.CHAVE_TOKEN,
+            (err: Error, decoder: any) => {
+                if (err) {
+                    return res.status(401).json({
+                        autenticado: false,
+                        mensagem: "Usuário não autorizado",
+                    });
+                }
+                // req.userId = decoder.userId;
+                prox();
             }
-            // req.userId = decoder.userId;
-            prox();
-        });
+        );
     };
 }
 
