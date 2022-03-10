95716d466ac52c5a278ff23b20715bbe559bedcb
Eduardo Manoel de Lara <52054453+Eduardo2712@users.noreply.github.com>
2022-03-09 21:37:48 -0300

    Atualização

--- prisma/migrations/20220219155525_migracao1/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `id_senha` on table `usuarios` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE `usuarios` MODIFY `id_senha` INTEGER NOT NULL,
-    MODIFY `data_nasc` DATE NULL;
--- prisma/migrations/20220309231326_banco/migration.sql
+++ prisma/migrations/20220309231326_banco/migration.sql
@@ -3,14 +3,20 @@ CREATE TABLE `anuncios` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `id_usuario` INTEGER NOT NULL,
     `id_tipo_anuncio` INTEGER NOT NULL,
-    `titulo` VARCHAR(255) NULL,
-    `descricao` TEXT NULL,
-    `valor` DOUBLE NULL,
-    `data_inicio_vigencia` DATETIME(0) NOT NULL,
-    `data_criado` DATETIME(0) NOT NULL,
-    `data_atualizado` DATETIME(0) NOT NULL,
-    `ativo` TINYINT NULL,
-    `patrocinado` TINYINT NULL,
+    `titulo` VARCHAR(255) NOT NULL,
+    `descricao` TEXT NOT NULL,
+    `valor` INTEGER NOT NULL,
+    `data_criado` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `data_atualizado` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `ativo` TINYINT NOT NULL,
+    `patrocinado` TINYINT NOT NULL,
+    `cep` VARCHAR(255) NULL,
+    `rua` VARCHAR(255) NOT NULL,
+    `cidade` VARCHAR(255) NOT NULL,
+    `numero` VARCHAR(255) NOT NULL,
+    `complemento` VARCHAR(255) NULL,
+    `bairro` VARCHAR(255) NOT NULL,
+    `estado` VARCHAR(255) NOT NULL,
 
     INDEX `id_tipo_anuncio`(`id_tipo_anuncio`),
     INDEX `id_usuario`(`id_usuario`),
@@ -24,27 +30,19 @@ CREATE TABLE `imagens` (
     `caminho` VARCHAR(255) NULL,
     `id_anuncio` INTEGER NOT NULL,
     `principal` BOOLEAN NULL,
-    `data_criado` DATETIME(0) NOT NULL,
-    `data_atualizado` DATETIME(0) NOT NULL,
+    `data_criado` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `data_atualizado` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
 
     INDEX `id_anuncio`(`id_anuncio`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `sequelizemeta` (
-    `name` VARCHAR(255) NOT NULL,
-
-    UNIQUE INDEX `name`(`name`),
-    PRIMARY KEY (`name`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
 CREATE TABLE `tipo_anuncios` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `nome` VARCHAR(255) NULL,
-    `data_criado` DATETIME(0) NOT NULL,
-    `data_atualizado` DATETIME(0) NOT NULL,
+    `data_criado` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `data_atualizado` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -54,10 +52,10 @@ CREATE TABLE `usuarios` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `nome` VARCHAR(255) NULL,
     `ativo` INTEGER NULL DEFAULT 1,
-    `id_senha` INTEGER NULL DEFAULT 1,
+    `id_senha` INTEGER NOT NULL,
     `email` VARCHAR(255) NULL,
     `telefone` VARCHAR(255) NULL,
-    `data_nasc` DATETIME(0) NULL,
+    `data_nasc` DATE NULL,
     `cpf` VARCHAR(255) NULL,
     `login` VARCHAR(255) NULL,
     `senha` VARCHAR(255) NULL,
@@ -71,11 +69,11 @@ CREATE TABLE `usuarios` (
 -- CreateTable
 CREATE TABLE `valores` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `valor` DOUBLE NULL,
+    `valor` INTEGER NULL,
     `ativo` INTEGER NULL DEFAULT 1,
     `id_anuncio` INTEGER NOT NULL,
-    `data_criado` DATETIME(0) NOT NULL,
-    `data_atualizado` DATETIME(0) NOT NULL,
+    `data_criado` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `data_atualizado` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
 
     INDEX `id_anuncio`(`id_anuncio`),
     PRIMARY KEY (`id`)
